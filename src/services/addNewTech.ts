400e079fdd0ef4049b5585076ee1cdb978e89927
Filipe Monteiro Fernandes <filipemonteirofernandes9@gmail.com>
2022-10-24 13:35:18 -0300

    feat: add type in requests

--- src/services/addNewTech.ts
+++ src/services/addNewTech.ts
@@ -2,6 +2,6 @@ import { iTech } from "../components/EditTechModal";
 import api from "./api";
 
 export const addNewTech =async (data:iTech) => {
-  const response = await api.post("users/techs", data);
+  const response = await api.post<iTech>("users/techs", data);
   return response
 }
--- src/services/editExistingTech.ts
+++ src/services/editExistingTech.ts
@@ -2,6 +2,6 @@ import { iTech } from "../components/EditTechModal";
 import api from "./api";
 
 export const editExistingTech = async (body:iTech, id: string) => {
-   const response = await api.put(`/users/techs/${id}`, body);
+   const response = await api.put<iTech>(`/users/techs/${id}`, body);
    return response;
 }
